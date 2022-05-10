438c82d20c4b92feca0cbe98468a25f5c898474b
lelis <lelis685@gmail.com>
2022-05-10 07:03:20 -0300

    test: add Parameterized Tests

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -1,9 +1,12 @@
 package com.healthycoderapp;
 
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +14,13 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BMICalculatorTest {
-    
 
-    @Test
-    public void should_returnTrue_when_dietRecommended() {
-        double weight = 89.0;
-        double height = 1.72;
+
+    @ParameterizedTest(name= "weight={0}, height={1}")
+    @CsvSource(value = {"89.0, 1.72", "95.0, 1.75"})
+    public void should_returnTrue_when_dietRecommended(Double coderWeight, Double coderHeight) {
+        double weight = coderWeight;
+        double height = coderHeight;
 
         boolean recommended = BMICalculator.isDietRecommended(weight, height);
 
@@ -24,9 +28,22 @@ public class BMICalculatorTest {
     }
 
 
-    @Test
-    public void should_returnFalse_when_dietNotRecommended() {
-        double weight = 50.0;
+    @ParameterizedTest(name= "weight={0}, height={1}")
+    @CsvFileSource(resources = "/diet-recommended-input-data.csv", numLinesToSkip = 1)
+    public void should_returnTrue_when_dietRecommended_usingCsvFile(Double coderWeight, Double coderHeight) {
+        double weight = coderWeight;
+        double height = coderHeight;
+
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+        assertTrue(recommended);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {40.0, 50.0, 30.6})
+    public void should_returnFalse_when_dietNotRecommended(Double coderWeight) {
+        double weight = coderWeight;
         double height = 1.72;
 
         boolean recommended = BMICalculator.isDietRecommended(weight, height);
